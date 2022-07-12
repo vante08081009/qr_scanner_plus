8b28dfa9b564f601109bfc39ba330972e3df1899
Tony <tony@lookingpet.com>
2022-07-12 18:25:47 +0800

    11

---
+++ example/android/app/src/main/kotlin/com/example/qr_scanner_plus_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qr_scanner_plus_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/qr_scanner_plus.dart
@@ -0,0 +1,13 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class QrScannerPlus {
+  static const MethodChannel _channel = MethodChannel('qr_scanner_plus');
+
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
