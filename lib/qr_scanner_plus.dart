dca5619b5d36628ada676beb8dc4be68e5a1dc16
Tony <tony@lookingpet.com>
2022-07-14 17:23:12 +0800

    fix

--- lib/qr_scanner_plus.dart
+++ lib/qr_scanner_plus.dart
@@ -115,34 +115,33 @@ class _BarcodeScannerViewState extends State<QrScannerPlusView> {
     saveResultCache(tmpBarcodes);
 
     //callback result every 0.5s
-    if (_timerCallbackResult?.isActive ?? false == false) {
-      _timerCallbackResult = Timer(const Duration(milliseconds: 300), () {
-        if (resultCache.isNotEmpty) {
-          if (widget.stop != true) {
-            widget.onResult.call(resultCache);
-          }
-
-          if (widget.debug == true) {
-            if (inputImage.inputImageData?.size != null &&
-                inputImage.inputImageData?.imageRotation != null) {
-              final painter = BarcodeDetectorDebugPainter(
-                  resultCache,
-                  inputImage.inputImageData!.size,
-                  inputImage.inputImageData!.imageRotation);
-
-              setState(() {
-                _customPaint = CustomPaint(painter: painter);
-              });
-            }
-          }
-        } else {
-          setState(() {
-            _customPaint = null;
-          });
+    _timerCallbackResult ??=
+        Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      if (resultCache.isNotEmpty) {
+        if (widget.stop != true) {
+          widget.onResult.call(resultCache);
         }
-      });
-    }
 
+        if (widget.debug == true) {
+          if (inputImage.inputImageData?.size != null &&
+              inputImage.inputImageData?.imageRotation != null) {
+            final painter = BarcodeDetectorDebugPainter(
+                resultCache,
+                inputImage.inputImageData!.size,
+                inputImage.inputImageData!.imageRotation);
+
+            setState(() {
+              _customPaint = CustomPaint(painter: painter);
+            });
+          }
+        }
+      } else {
+        setState(() {
+          _customPaint = null;
+        });
+      }
+    });
+    setState(() {});
     _isBusy = false;
   }
 }
