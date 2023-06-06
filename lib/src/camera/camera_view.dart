cdae122c33b9a31aaf7c3f0ae1631c08c15418c7
tony <tony@zuzuche.com>
2023-06-06 18:17:22 +0800

    change  ResolutionPreset  to veryHigh

--- lib/src/camera/camera_view.dart
+++ lib/src/camera/camera_view.dart
@@ -275,7 +275,7 @@ class _CameraViewState extends State<CameraView> with WidgetsBindingObserver {
     final camera = cameras[_cameraIndex];
     cameraController = CameraController(
       camera,
-      ResolutionPreset.medium,
+      ResolutionPreset.veryHigh,
       enableAudio: false,
     );
     focusPoint = FocusPoint(cameraController!);
